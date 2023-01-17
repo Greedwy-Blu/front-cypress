5ecae92d9891b759ab928a85509997e27d095a00
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-17 16:09:04 -0300

    feat: projeto inicial

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: 'standard',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'no-unused-expressions': 'off',
+        'no-unused-vars': 'off',
+        camelcase: 'off',
+        'no-use-before-define': 'off',
+        'prefer-const': 'off',
+        'n/no-path-concat': 'off',
+    },
+}
