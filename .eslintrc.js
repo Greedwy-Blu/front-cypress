1a47b1bf6236b693b1a0528f0b127f8d3241bd37
Greedwy-Blu <greedwyblu@gmail.com>
2023-01-17 18:49:49 -0300

    feat: adicionando arquivos do cypress para aprender como funciona

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,16 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: 'standard',
+    plugins: ['cypress'],
+    extends: ['standard', 'plugin:cypress/recommended'],
     overrides: [],
     parserOptions: {
         ecmaVersion: 'latest',
     },
     rules: {
-        'no-unused-expressions': 'off',
-        'no-unused-vars': 'off',
-        camelcase: 'off',
-        'no-use-before-define': 'off',
-        'prefer-const': 'off',
-        'n/no-path-concat': 'off',
+        singleQuote: true,
+        semi: true,
+        'jest/expect-expect': 'off',
         indent: 'off',
     },
 }
